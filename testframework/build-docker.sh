f204047df35a398f507fb49f7df6e2cc1843a753
Amir Blum <amirgiraffe@gmail.com>
2025-08-28 12:10:57 +0300

    ci: use docker base imgae with correct go version

--- testframework/build-docker.sh
+++ testframework/build-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get Go version from command line argument, default to 1.24
+GO_VERSION=${1:-1.24}
+
 # Build Docker image from parent directory context
 cd ..
-docker build -f testframework/Dockerfile -t go-rtml-test:latest .
+docker build --build-arg GO_VERSION=${GO_VERSION} -f testframework/Dockerfile -t go-rtml-test:latest .
